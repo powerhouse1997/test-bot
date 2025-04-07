04c9eb397794b948813c899ddb28ed6f6844dc7d
Nikhil Raut <34471968+powerhouse1997@users.noreply.github.com>
2025-04-07 12:14:03 +0530

    Create database.py

---
+++ bot/database.py
@@ -0,0 +1,22 @@
+from tinydb import TinyDB, Query
+
+db = TinyDB('db.json')
+reminders_table = db.table('reminders')
+todos_table = db.table('todos')
+
+def add_reminder(chat_id, time, text):
+    reminders_table.insert({'chat_id': chat_id, 'time': time, 'text': text})
+
+def get_reminders(chat_id):
+    return reminders_table.search(Query().chat_id == chat_id)
+
+def add_todo(chat_id, task):
+    todos_table.insert({'chat_id': chat_id, 'task': task, 'done': False})
+
+def get_todos(chat_id):
+    return todos_table.search((Query().chat_id == chat_id) & (Query().done == False))
+
+def complete_todo(chat_id, index):
+    todos = get_todos(chat_id)
+    if 0 <= index < len(todos):
+        todos_table.update({'done': True}, doc_ids=[todos[index].doc_id])

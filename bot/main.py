53ba4581720af933c209fd21ddbc6fcc7c35a4b0
Nikhil Raut <34471968+powerhouse1997@users.noreply.github.com>
2025-04-11 12:48:03 +0530

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -60,7 +60,7 @@ async def main():
     await app.run_webhook(
         listen="0.0.0.0",
         port=int(os.environ.get('PORT', 8000)),
-        webhook_url=DOMAIN
+        webhook_url=WEBHOOK_URL
     )
 
 if __name__ == "__main__":

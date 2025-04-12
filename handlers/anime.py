a83838da1f602acac5ceaaf641b87753e4f61bcc
Nikhil Raut <34471968+powerhouse1997@users.noreply.github.com>
2025-04-12 23:49:59 +0530

    Update anime.py

--- handlers/anime.py
+++ handlers/anime.py
@@ -1,8 +1,7 @@
 from aiogram import Router, types
 from aiogram.filters import Command
 from jikan_api import search_jikan
-from aiogram import Router
-
+import html
 
 router = Router()
 
@@ -12,20 +11,31 @@ async def cmd_anime(message: types.Message):
     if len(args) < 2:
         await message.reply("Usage: /a <anime title>")
         return
+
     results = await search_jikan("anime", args[1])
     if not results:
         await message.reply("No anime found.")
         return
+
     anime = results[0]
+
+    title = html.escape(anime['title'])
+    synopsis = html.escape(anime.get('synopsis', ''))[:500]
+    score = anime.get('score', 'N/A')
+    episodes = anime.get('episodes', 'Unknown')
+    url = html.escape(anime['url'])
+
     caption = (
-        f"🎬 <b>{anime['title']}</b>\n"
-        f"⭐ Score: {anime.get('score', 'N/A')}\n"
-        f"📺 Episodes: {anime.get('episodes', 'Unknown')}\n"
-        f"🔗 <a href='{anime['url']}'>More Info</a>\n\n"
-        f"{anime.get('synopsis', '')[:500]}..."
+        f"🎬 <b>{title}</b>\n"
+        f"⭐ Score: {score}\n"
+        f"📺 Episodes: {episodes}\n"
+        f"🔗 <a href='{url}'>More Info</a>\n\n"
+        f"{synopsis}..."
     )
+
     await message.bot.send_photo(
         chat_id=message.chat.id,
         photo=anime['images']['jpg']['large_image_url'],
-        caption=caption
+        caption=caption,
+        parse_mode="HTML"
     )

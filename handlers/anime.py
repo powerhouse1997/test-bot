b3dc9df8f116ca384b7729410e1efe293a2fc50d
Nikhil Raut <34471968+powerhouse1997@users.noreply.github.com>
2025-04-12 22:50:38 +0530

    Update anime.py

--- handlers/anime.py
+++ handlers/anime.py
@@ -1,24 +1,29 @@
-from aiogram import types
+from aiogram import Router, types
 from aiogram.filters import Command
 from jikan_api import search_jikan
 
-def register_anime(dp):
-    @dp.message(Command("a"))
-    async def cmd_anime(message: types.Message):
-        args = message.text.split(maxsplit=1)
-        if len(args) < 2:
-            await message.reply("Usage: /a <anime title>")
-            return
-        results = await search_jikan("anime", args[1])
-        if not results:
-            await message.reply("No anime found.")
-            return
-        anime = results[0]
-        caption = (
-            f"🎬 <b>{anime['title']}</b>\n"
-            f"⭐ Score: {anime.get('score', 'N/A')}\n"
-            f"📅 Aired: {anime.get('aired', 'Unknown')}\n"
-            f"🔗 <a href='{anime['url']}'>More Info</a>\n\n"
-            f"{anime.get('synopsis', '')[:500]}..."
-        )
-        await message.bot.send_photo(message.chat.id, anime['image_url'], caption=caption, parse_mode="HTML")
+router = Router()
+
+@router.message(Command("a"))
+async def cmd_anime(message: types.Message):
+    args = message.text.split(maxsplit=1)
+    if len(args) < 2:
+        await message.reply("Usage: /a <anime title>")
+        return
+    results = await search_jikan("anime", args[1])
+    if not results:
+        await message.reply("No anime found.")
+        return
+    anime = results[0]
+    caption = (
+        f"🎬 <b>{anime['title']}</b>\n"
+        f"⭐ Score: {anime.get('score', 'N/A')}\n"
+        f"📺 Episodes: {anime.get('episodes', 'Unknown')}\n"
+        f"🔗 <a href='{anime['url']}'>More Info</a>\n\n"
+        f"{anime.get('synopsis', '')[:500]}..."
+    )
+    await message.bot.send_photo(
+        chat_id=message.chat.id,
+        photo=anime['images']['jpg']['large_image_url'],
+        caption=caption
+    )
